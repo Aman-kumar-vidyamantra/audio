585a8c09ffad8212d36135c87c4ef31b1d294449
Aman-kumar-vidyamantra <aman.kumar@vidyamantra.com>
2023-10-14 14:18:54 +0530

    updated buttons

--- app.vue
+++ app.vue
@@ -57,24 +57,14 @@
           </i>
         </button>
         <button onclick="volumeUp()">
-          <i class="bx bx-skip-next">
+          volume -
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="black" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-
-          </i>
+          
         </button>
         <button onclick="volumeUp()">
-          <i class="bx bx-skip-next">
+         Volume +
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="black" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-
-          </i>
+        
         </button>
 
       </div>
